0aa1bb462dddbd4040a1822c13153b80d53fc17c
Wangmingyang0604 <wangmingyang0604@foxmail.com>
2025-12-09 23:35:30 +0800

    Add files via upload

--- WGS/GWAS.sh
+++ WGS/GWAS.sh
@@ -10,4 +10,4 @@ gemma -bfile Poly4 -k ${input}/output/result.sXX.txt -c cov.txt -lmm 2 -p phe.tx
 plink --bfile Poly4 --snps chr18:51384660:IG --recode12 --out test
 awk '{print 1,$7,$8}' test.ped >1.txt
 awk '{print ($1==1 && $2==2 && $3==2) ? "1 0 0" : ($1==1 && $2==1 && $3==2) ? "0 1 0" : ($1==1 && $2==1 && $3==1) ? "0 0 1" : $0}' 1.txt > gene.txt
-gemma --bfile Poly4 -k ${input}/output/result.sXX.txt -c cov2.txt -lmm 2 -p new_phenotype.txt -o condition_result
+gemma --bfile Poly4 -k ${input}/output/result.sXX.txt -c cov.txt -lmm 2 -p new_phenotype.txt -o condition_result
